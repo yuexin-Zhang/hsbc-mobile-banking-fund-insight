4bb4b41ce9957a52e06ee2c1e33f4afa94b5b02b
yuexin-Zhang <zyxfwy@gmail.com>
2026-02-09 17:43:24 +0800

    fix import tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      css: {
+        postcss: './postcss.config.js',
+      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
